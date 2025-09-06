c49f070a6c56923a73a5b5102f8c6c1e7f19e40e
andrew barbanera <andrewbarbanera@gmail.com>
2025-09-06 16:39:32 -0600

    adding log

--- metrics/src/main/java/com/barbanera/metrics/services/BlockingService.java
+++ metrics/src/main/java/com/barbanera/metrics/services/BlockingService.java
@@ -17,6 +17,7 @@ public class BlockingService {
     private final MetricsPostgresRepository metricsPostgresRepository;
 
     public List<BlockingMetric> getMetrics() {
+        log.info("Finding all blocking metrics");
         return metricsPostgresRepository.findAll();
     }
 
